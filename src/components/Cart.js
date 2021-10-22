e57fbf08c185450a5d6c3ba2350d859becfdaee3
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2021-10-23 04:27:27 +0530

    can't sleep , react is way too interesting .

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,30 @@
-import React from 'react';
-
+import React from "react";
 
 function Cart() {
-    return (
-        <div>
-            <h1>Cart</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Cart</h1>
+      Total Cost: <br />
+      Toatal Items:
+      <div>
+        <h3 class="card">
+          Nike Air Force : <span>7 UK</span>
+          <h4>Quantity : 2</h4>
+          Cost: 200 * 2 : 400
+        </h3>
+        <h3 class="card">
+          Nike Air Force : <span>7 UK</span>
+          <h4>Quantity : 2</h4>
+          Cost: 200 * 2 : 400
+        </h3>
+        <h3 class="card">
+          Nike Air Force : <span>7 UK</span>
+          <h4>Quantity : 2</h4>
+          Cost: 200 * 2 : 400
+        </h3>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,7 +10,7 @@ function ProductCard({ product }) {
         </Link>
         <div>
           <h2>{product.name}</h2>
-          <h3>${product.price}</h3>
+          <h3> Rs. {product.price}</h3>
         </div>
       </div>
     </div>
